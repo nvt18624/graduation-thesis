aee9121fce62ed1a9332bbd9feae73eeef23be28
tn18624 <tn18624@practise.com>
2026-04-11 13:50:13 +0700

    fix name file for consitency debug and config

--- src/config/file/P0_gitlab_server_install.sh
+++ src/config/file/P0_gitlab_server_install.sh
@@ -7,5 +7,3 @@ curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.de
 mv gitlab.rb /etc/gitlab/gitlab.rb
 sudo gitlab-ctl reconfigure
 
-## check connect to server ad
-# sudo gitlab-rake gitlab:ldap:check
